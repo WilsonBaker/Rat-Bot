7a3b0c5376922d1f6fa5380a2521c1fedfa4191c
Wilson Baker <49139851+WilsonBaker@users.noreply.github.com>
2021-09-03 20:29:37 +1200

    Basic server setup

--- bot.js
+++ bot.js
@@ -1,12 +1,17 @@
-console.log("hello world");
-const Discord = require('discord.js');
-const { token } = require('./config.json')
+const { Client, Intents } = require('discord.js');
+const { token } = require('./config.json');
 
-const client = new Discord.Client();
-client.login(token);
+// New client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-client.on('ready', readyDiscord);
-
-function readyDiscord() {
+// When client ready, run code (only once though)
+client.once('ready', () => {
     console.log("BRuh");
+});
+
+// Login to Discord with client token
+try{
+    client.login(token);
+} catch(err) {
+    console.log(err);
 }
