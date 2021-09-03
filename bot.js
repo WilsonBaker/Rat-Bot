21197d8f560f1d0ea8ad121dabdedb9a6912d9ba
Wilson Baker <49139851+WilsonBaker@users.noreply.github.com>
2021-09-03 19:27:28 +1200

    Added discord API token

--- bot.js
+++ bot.js
@@ -1,1 +1,12 @@
-console.log("hello world")
+console.log("hello world");
+const Discord = require('discord.js');
+const { token } = require('./config.json')
+
+const client = new Discord.Client();
+client.login(token);
+
+client.on('ready', readyDiscord);
+
+function readyDiscord() {
+    console.log("BRuh");
+}
